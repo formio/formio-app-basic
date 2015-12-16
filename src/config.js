f66a48e3a85e666141e87e3520562435ea246e80
Travis Tidwell <travis@form.io>
2015-12-15 21:33:29 -0600

    Fixing the config file.

--- src/config.js
+++ src/config.js
@@ -7,9 +7,12 @@ location.search.substr(1).split("&").forEach(function(item) {
   query[item.split("=")[0]] = item.split("=")[1] && decodeURIComponent(item.split("=")[1]);
 });
 
+APP_URL = query.appUrl || APP_URL;
+API_URL = query.apiUrl || API_URL;
+
 angular.module('formioAppBasic').constant('AppConfig', {
-  appUrl: query.appUrl || APP_URL,
-  apiUrl: query.apiUrl || API_URL,
+  appUrl: APP_URL,
+  apiUrl: API_URL,
   forms: {
     userLoginForm: APP_URL + '/user/login',
     userRegisterForm: APP_URL + '/user/register'
