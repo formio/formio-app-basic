776ea0da71bec5840a9206f5ad4a788307b57f8d
Travis Tidwell <codertravist@gmail.com>
2016-06-03 19:11:52 -0500

    Added much better documentation in the User.js

--- src/app/resources/User.js
+++ src/app/resources/User.js
@@ -12,28 +12,82 @@ angular.module('formioAppBasic')
       $get: function() { return null; },
 
       /**
-       * Allow for nested resources by setting the parent to another resource.
+       * The parent is name of the Resource field within your resource that points to the parent resource. This ultimately
+       * is passed along as an array to the "parents" paramter below.
        */
-      parent: null,
+      parent: '',
+      
+      /**
+       * Allows you to provide multiple parents or the names of the resource fields that serve as the parent of this resource. 
+       * For example, if you wish your users to be within Groups, and you have a Resource and Resource field within the User 
+       * where the API key is 'group', you would provide the following. ['group']
+       */
+      parents: [],
+      
+      /**
+       * This allows you to nest your resources within a UI Router path. For example, if you wish users to be within groups, and
+       * you have a Group resource, you could provide the following base.  'group.'
+       */
+      base: '',
 
       /**
-       * Allow you to change the template for any view of this resource.
+       * Allow you to change the template for any view of this resource. You can override any of these templates by copying the 
+       * corresponding template within the bower_components/ng-formio-helper/src/templates/resource folder. You can copy any of
+       * those templates, paste that to your own views folder, and then provide the new path to that template below.
        */
       templates: {
+        
+        /**
+         * The index view for this resource where all of the resources can be seen. By default, the ngFormioHelper will set this
+         * to a view of the data grid. 
+         */
         index: '',
-        view: '',
+        
+        /**
+         * This is the create view. This typically will contain the <formio> directive to allow you to create a new Resource.
+         */
         create: '',
+        
+        /**
+         * The abstract view is the view that wraps the "view", "edit", and "delete" views and typically provides the tabs where you
+         * can navigate between those views. 
+         */
+        abstract: '',
+        
+        /**
+         * The view is the view page for the resource.
+         */
+        view: '',
+        
+        /**
+         * This is the edit view. This will have the <formio> directive with the submission provided into the directive.
+         */
         edit: '',
+        
+        /**
+         * This is the delete view, which is really just a confirmation page asking if you would like to delete the Resource.
+         */
         delete: ''
       },
 
       /**
-       * Provide customer parameters to each of the operations for this resource.
+       * Provide customer parameters to each of the operations for this resource. This allows you to pass in custom objects into 
+       * each state. For example, lets say you wish to pass in the whole object of "customer" into an Order resource, you could provide
+       * the following to the view params.
+       *    
+       *   params: {
+       *     view: {customer: null}
+       *   }
+       * 
+       * This would then allow you to call that state by passing in the whole object.
+       * 
+       *   $state.go('order.view', {customer: customer});
        */
       params: {
         index: {},
-        view: {},
         create: {},
+        abstract: {},
+        view: {},
         edit: {},
         delete: {}
       },
@@ -42,14 +96,33 @@ angular.module('formioAppBasic')
        * Provide custom controllers for each operation on a resource.
        */
       controllers: {
-        index: null,
+        index: ['$scope', function($scope) {
+          // You can register when an item in the index has been clicked by doing the following.
+          $scope.$on('rowView', function(event, resource) {
+            console.log(resource);
+          });
+        }],
         abstract: null,
-        view: null,
+        view: ['$scope', function($scope) {
+          // To register when the form is loaded, you can use the following.
+          $scope.currentResource.loadFormPromise.then(function(form) {
+            console.log(form);
+          });
+          
+          // To register when a resource is loaded, you can use the following.
+          $scope.currentResource.loadSubmissionPromise.then(function(resource) {
+            console.log(resource);
+          });
+        }],
         create: ['$scope', function($scope) {
           $scope.$on('formSubmission', function(err, submission) {
             // A submission has been made... Do something...
             console.log(submission);
           });
+          
+          // You can decide to tell the Formio Helper to allow this controller do the 
+          // submission handling by passing the following back to the helper.
+          // return {handle: true};
         }],
         edit: ['$scope', function($scope) {
           $scope.$on('formSubmission', function(err, submission) {
