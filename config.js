2925597f101c6f36d354a152a3f2182b7fc1d8cf
Travis Tidwell <travis@form.io>
2017-02-02 22:15:33 -0600

    New app build.

---
+++ config.js
@@ -0,0 +1,27 @@
+var APP_URL = 'https://yourapp.form.io';
+var API_URL = 'https://api.form.io';
+
+// Parse query string
+var query = {};
+location.search.substr(1).split("&").forEach(function(item) {
+  query[item.split("=")[0]] = item.split("=")[1] && decodeURIComponent(item.split("=")[1]);
+});
+
+APP_URL = query.appUrl || APP_URL;
+API_URL = query.apiUrl || API_URL;
+
+angular.module('formioAppBasic').constant('AppConfig', {
+  appUrl: APP_URL,
+  apiUrl: API_URL,
+  forms: {
+    userForm: APP_URL + '/user',
+    userLoginForm: APP_URL + '/user/login',
+    userRegisterForm: APP_URL + '/user/register'
+  },
+  resources: {
+    user: {
+      form: APP_URL + '/user',
+      resource: 'UserResource'
+    }
+  }
+});
